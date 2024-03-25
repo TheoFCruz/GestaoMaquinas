76c9290386d5e69e74b621e071c730c3676cbb6e
Theo Cruz <theofonsecacruz@gmail.com>
2024-03-25 15:26:15 -0300

    Logica inicial de inscrito

---
+++ app/configs/broker_configs.py
@@ -0,0 +1,7 @@
+mqtt_broker_configs: dict = {
+    'HOST': 'localhost',
+    'PORT': 1883,
+    'CLIENT_NAME': 'esp_cpejr_sub',
+    'KEEPALIVE': 60,
+    'TOPIC': 'gestao/teste'
+}
---
+++ app/main/mqtt_connection/mqtt_client_connection.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+
+class MqttClientConnection:
+    def __init__(self,
+                 broker_ip: str,
+                 port: int,
+                 client_name: str,
+                 keepalive=60):
+        self.__broker_ip = broker_ip
+        self.__port = port
+        self.__client_name = client_name
+        self.__keepalive = keepalive
+        self.__mqtt_client = None
+
+    def start_connection(self) -> None:
+        mqtt_client = mqtt.Client(self.__client_name)
+        mqtt_client.connect(host = self.__broker_ip,
+                            port = self.__port,
+                            keepalive = self.__keepalive)
+
+        self.__mqtt_client = mqtt_client
+
+        
